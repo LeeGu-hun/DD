24943b31afdaa9890c7a4ffae98c490e1447e21c
Kim-ji-ho <jiho9312@naver.com>
2020-04-21 16:18:37 +0900

    sdasdasda

---
+++ src/main/java/org/dd/controller/reportController.java
@@ -0,0 +1,33 @@
+package org.dd.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/test/")
+public class reportController {
+
+	@GetMapping("/report")
+	public void home() {
+		
+	}
+}
+		
+		
+		
+		
+		
+		
+		
